c796efaae0571df7400dd7db4cf44e9b82d163c7
karimsalehabbas <karimaazz0099@gmail.com>
2025-06-22 00:52:55 +0300

    Add project files.

---
+++ assignment02/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace assignment02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
